27983e7821e061f00265f73ff0d70e9e14f01bcc
Linux1975 <gianluca.villani@gmail.com>
2018-12-02 01:05:02 +0100

    extended the main recipe , apache.rb with new features

--- webapp-apptio/recipes/apache.rb
+++ webapp-apptio/recipes/apache.rb
@@ -8,19 +8,51 @@ bash 'setup-apache-yum' do
     ignore_failure false
     code <<-EOH
     yum install -y httpd
+    cd /root
     wget https://s3-us-west-2.amazonaws.com/techops-interview-webapp/webapp.tar.gz #create an S3 bucket in my account with public and dist folder
     tar xvzf webapp.tar.gz
     cp -p public/s* /var/www/html/
-    cp -p public/index.html /var/www/html/
     service httpd start
     chkconfig httpd on
     EOH
 end
 
-# Provision index.html to ensure ELB health check passes
-template "/var/www/html/index.html" do
-    source "apache-default-index.erb"
+# Raplce existing configuration files with configuration from unified templates
+# Get the Chef::CookbookVersion for this cookbook
+cb = run_context.cookbook_collection['webapp']
+
+# Remove default httpd config files
+bash 'httpd-remove-existing-config' do
+    user 'root'
+    ignore_failure false
+    code <<-EOH
+    rm -f /etc/httpd/conf/*.*
+    rm -f /etc/httpd/conf.d/*.*
+    rm -f /etc/httpd/conf.modules.d/*.*
+    EOH
+end
+
+# Loop through and copy all http configuration templates
+cb.manifest['templates'].each do |cbf|
+  filepath = cbf['path']
+
+  # Only parse unified specific config templates
+  next if not filepath.include? "templates/webapp/"
+  filepath = filepath.sub("templates/webapp/httpd/", '')
+
+template "/etc/httpd/#{filepath}" do
+    source "webapp/httpd/#{filepath}"
+    mode 0755
     owner "ec2-user"
     group "ec2-user"
-    mode 0644
+  end
+end
+
+
+bash "restart_apache" do
+  user 'root'
+  ignore_failure false
+  code <<-EOL
+    service httpd restart
+  EOL
 end
