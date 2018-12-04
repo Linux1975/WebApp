6a1bc3564b70c07c4a6e4b5748af03378a3d0e5f
Linux1975 <gianluca.villani@gmail.com>
2018-12-04 11:18:33 +0100

    modified deploy recipe the app path was wrong

--- webapp-apptio/recipes/deploy.rb
+++ webapp-apptio/recipes/deploy.rb
@@ -4,13 +4,14 @@
 #app_path is the path to clone the repository to. If the path doesn't exist on the instance, AWS OpsWorks Stacks creates it.
 
 app = search(:aws_opsworks_app).first
-app_path = "/srv/#{app['shortname']}""
+app_path = "/var/www/html/"
 
 package "git" do
   options "--force-yes" if node["platform"] == "ubuntu" && node["platform_version"] == "16.04"
 end
 
 #git gets the source code from the specified repository and branch if there is a delta
+
  git app_path do
     repository app["app_source"]["url"]
     revision app["app_source"]["revision"]
